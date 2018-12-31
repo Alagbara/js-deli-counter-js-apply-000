be9da9c8da28273c0e6e27d15b5d0659484eb7e3
Moses Olawoyin <m.bastford@gmail.com>
2018-12-31 20:24:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nowServing(katzDeliLine){
   if(!katzDeliLine.length){
     return 'There is nobody waiting to be served!'
   } else {
-  return `Currently serving ${katzDeliLine.shift()}.`
+    return `Currently serving ${katzDeliLine.shift()}.`
   }
   //if( nowServing = 0){
     //return ("There is nobody waiting to be served!")
