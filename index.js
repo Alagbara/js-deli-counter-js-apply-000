446e00296f55b063c71eeaba5a1f840e85131df5
Moses Olawoyin <m.bastford@gmail.com>
2018-12-31 20:28:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function nowServing(katzDeliLine){
 function currentLine(line){
   console.log(line)
   if(line.length === 0){
-    return " The line is currently empty."
+    return "The line is currently empty."
   }
   return currentLine
 }
